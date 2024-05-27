49d605b7ee679977e398e3fa8b6e25743f6e34cc
poojaMalles <ppooja6372@gmail.com>
2024-05-27 21:21:54 +0530

    first commit

---
+++ src/main/java/com/excelr/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.excelr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+}
